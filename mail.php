9ad0a6f1b77be010739f424d735c82114fb3c910
SMauhourat <santiagomauhourat@hotmail.com>
2022-07-26 19:54:46 -0300

    send contact mail

--- mail.php
+++ mail.php
@@ -1,4 +1,8 @@
 <?php
+    require("class.phpmailer.php");
+    require("class.smtp.php");
+
+
     $to = "youremail.com"; // replace this email with yours
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
@@ -11,10 +15,70 @@
     $fields["subject"] = $_REQUEST['subject'];
     $fields["message"] = $_REQUEST['message'];
 
-    $body = "Message From" $_SERVER['SERVER_NAME']":\n\n <br><br>";
-    $body .= '<strong>Name</strong> : '.$fields['name']. '<br><br>';
+
+    $body = 'Mensaje Web <br><br>';
+    $body .= '<strong>Name</strong> : '.$fields['name'].'<br><br>';
     $body .= '<strong>Email</strong> : '.$fields['email']. '<br><br>';
     $body .= '<strong>Subject</strong> : '.$fields['subject']. '<br><br>';
     $body .= '<strong>Message</strong> : <br><br>'.$fields['message']. '<br>';
 
-    $send = mail($to, $subject, $body, $headers);
+    $bodyFlat = 'Mensaje Web';
+    $bodyFlat .= 'Name : '.$fields['name'];
+    $bodyFlat .= 'Email : '.$fields['email'];
+    $bodyFlat .= 'Subject : '.$fields['subject'];
+    $bodyFlat .= 'Message : <br><br>'.$fields['message'];
+
+    // echo $body;
+
+     $EmailTo = "info@demarcar.com.ar";
+     // $Subject = "Contacto Web - adhentux.com";
+
+    // Datos de la cuenta de correo utilizada para enviar vía SMTP
+    $smtpHost = "c1561680.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+    $smtpUsuario = "web@demarcar.com.ar";  // Mi cuenta de correo
+    $smtpClave = "A*WMSP77pA";  // Mi contraseña
+
+    //$send = mail($to, $subject, $body, $headers);
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->Port = 465; 
+    $mail->SMTPSecure = 'ssl';
+    $mail->IsHTML(true); 
+    $mail->CharSet = "utf-8";
+
+    // VALORES A MODIFICAR //
+    $mail->Host = $smtpHost; 
+    $mail->Username = $smtpUsuario; 
+    $mail->Password = $smtpClave;    
+
+    $mail->From = $from; // Email desde donde envío el correo.
+    $mail->FromName = $name;
+    $mail->AddAddress($EmailTo); // Esta es la dirección a donde enviamos los datos del formulario
+    
+    $mail->Subject = "Demarcar.com.ar - Contacto Web"; // Este es el titulo del email.
+    $mensajeHtml = nl2br($body);
+    $mail->Body = "{$mensajeHtml}"; // Texto del email en formato HTML
+    $mensajeFlat = nl2br($bodyFlat);
+    $mail->AltBody = "{$mensajeFlat}";
+    
+    // echo $body;
+    
+    // FIN - VALORES A MODIFICAR //
+    
+    $success = $mail->Send();
+
+    echo $success;
+
+    // // redirect to success page
+    // if ($success && $errorMSG == ""){
+    //     echo "success";
+    // }else{
+    //     if($errorMSG == ""){
+    //         echo "Ha ocurrido un error :(";
+    //     } else {
+    //         echo $errorMSG;
+    //     }
+    // }
+
+    ?>
